4cf148ed09f82161924aca8a6ba41e7e33d0dc7e
sguazt <marco.guazzone@gmail.com>
2015-04-30 19:29:26 +0200

    Moved template definitions inside header files (instead of keeping them in external .tpp file)

--- include/fl/anfis/engine.h
+++ include/fl/anfis/engine.h
@@ -29,7 +29,12 @@
 #include <fl/anfis/nodes.h>
 #include <fl/Engine.h>
 #include <fl/fuzzylite.h>
+#include <fl/macro.h>
+#include <fl/rule/RuleBlock.h>
+#include <fl/variable/InputVariable.h>
+#include <fl/variable/OutputVariable.h>
 #include <map>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -201,10 +206,160 @@ private:
 	bool isLearning_; ///< \c true if the ANFIS is in the learning modality
 }; // Engine
 
+
+////////////////////////
+// Template definitions
+////////////////////////
+
+
+template <typename InputIterT,
+		  typename OutputIterT,
+		  typename RuleBlockIterT>
+Engine::Engine(InputIterT inputFirst, InputIterT inputLast,
+			   OutputIterT outputFirst, OutputIterT outputLast,
+			   RuleBlockIterT ruleBlockFirst, RuleBlockIterT ruleBlockLast,
+			   const std::string& name)
+{
+	this->setInputVariables(inputFirst, inputLast);
+	this->setOutputVariables(outputFirst, outputLast);
+	this->setRuleBlocks(ruleBlockFirst, ruleBlockLast);
+	this->setName(name);
+	this->setHasBias(false);
+
+	this->build();
+}
+
+template <typename IterT>
+void Engine::setInputVariables(IterT first, IterT last)
+{
+	//inputs_.clear();
+	//inputs_.assign(first, last);
+	BaseType::setInputVariables(std::vector<fl::InputVariable*>());
+	while (first != last)
+	{
+		this->addInputVariable(*first);
+		++first;
+	}
+}
+
+template <typename IterT>
+void Engine::setOutputVariables(IterT first, IterT last)
+{
+	//outputs_.clear();
+	//outputs_.assign(first, last);
+	BaseType::setOutputVariables(std::vector<fl::OutputVariable*>());
+	while (first != last)
+	{
+		this->addOutputVariable(*first);
+		++first;
+	}
+}
+
+template <typename IterT>
+void Engine::setRuleBlocks(IterT first, IterT last)
+{
+	//ruleBlocks_.clear();
+	//ruleBlocks_.assign(first, last);
+	BaseType::setRuleBlocks(std::vector<fl::RuleBlock*>());
+	while (first != last)
+	{
+		this->addRuleBlock(*first);
+		++first;
+	}
+}
+
+template <typename InputIterT,
+		  typename OutputIterT,
+		  typename RuleBlockIterT>
+void Engine::build(InputIterT inputFirst, InputIterT inputLast,
+				   OutputIterT outputFirst, OutputIterT outputLast,
+				   RuleBlockIterT ruleBlockFirst, RuleBlockIterT ruleBlockLast)
+{
+	this->clear();
+
+	//inputs_.assign(inputFirst, inputLast);
+	this->setInputVariables(inputFirst, inputLast);
+	//outputs_.assign(outputFirst, outputLast);
+	this->setOutputVariables(outputFirst, outputLast);
+	//ruleBlocks_.assign(ruleBlockFirst, ruleBlockLast);
+	this->setRuleBlocks(ruleBlockFirst, ruleBlockLast);
+
+	this->build();
+}
+
+template <typename IterT>
+void Engine::setBias(IterT first, IterT last)
+{
+	const std::size_t ni = outputNodes_.size();
+
+	std::size_t i = 0;
+	while (first != last && i < ni)
+	{
+		outputNodes_[i]->setBias(*first);
+
+		++first;
+		++i;
+	}
+
+	//FIXME: decide if the remaining biases should be set to zero or left untouched
+	//for (; i < ni; ++i)
+	//{
+	//	outputNodes_[i]->setBias(0);
+	//}
+}
+
+template <typename IterT>
+void Engine::setInputValues(IterT first, IterT last)
+{
+	std::vector<InputNode*>::iterator nodeIt = inputNodes_.begin();
+	std::vector<InputNode*>::iterator nodeEndIt = inputNodes_.end();
+
+	while (first != last && nodeIt != nodeEndIt)
+	{
+		(*nodeIt)->getInputVariable()->setValue(*first);
+
+		++first;
+		++nodeIt;
+	}
+
+	if (first != last || nodeIt != nodeEndIt)
+	{
+		FL_THROW2(std::invalid_argument, "Wrong number of inputs");
+	}
+}
+
+template <typename IterT>
+std::vector<fl::scalar> Engine::eval(IterT first, IterT last)
+{
+	this->setInputValues(first, last);
+	return this->eval();
+}
+
+template <typename IterT>
+std::vector<fl::scalar> Engine::evalTo(IterT first, IterT last, LayerCategory layer)
+{
+	this->setInputValues(first, last);
+	return this->evalTo(layer);
+}
+
+template <typename IterT>
+std::vector<fl::scalar> Engine::evalLayer(IterT first, IterT last)
+{
+	std::vector<fl::scalar> res;
+	while (first != last)
+	{
+		Node* p_node = *first;
+
+		FL_DEBUG_ASSERT( p_node );
+
+		res.push_back(p_node->eval());
+
+		++first;
+	}
+	return res;
+}
+
 }} // Namespace fl::anfis
-
-
-#include "fl/anfis/engine.tpp"
 
 
 #endif // FL_ANFIS_ENGINE_H
--- include/fl/dataset.h
+++ include/fl/dataset.h
@@ -29,6 +29,7 @@
 #include <cstddef>
 #include <fl/macro.h>
 #include <fl/fuzzylite.h>
+#include <stdexcept>
 #include <vector>
 
 
@@ -134,10 +135,247 @@ private:
 	EntryContainer entries_;
 };
 
+
+////////////////////////
+// Template definitions
+////////////////////////
+
+
+////////////////
+// DataSetEntry
+////////////////
+
+
+template <typename ValueT>
+DataSetEntry<ValueT>::DataSetEntry()
+{
+}
+
+template <typename ValueT>
+template <typename InIterT, typename OutIterT>
+DataSetEntry<ValueT>::DataSetEntry(InIterT inFirst, InIterT inLast,
+								   OutIterT outFirst, OutIterT outLast)
+: in_(inFirst, inLast),
+  out_(outFirst, outLast)
+{
+}
+
+template <typename ValueT>
+template <typename IterT>
+void DataSetEntry<ValueT>::setInputs(IterT first, IterT last)
+{
+	in_.clear();
+	in_.assign(first, last);
+}
+
+template <typename ValueT>
+template <typename IterT>
+void DataSetEntry<ValueT>::setOutputs(IterT first, IterT last)
+{
+	out_.clear();
+	out_.assign(first, last);
+}
+
+template <typename ValueT>
+std::size_t DataSetEntry<ValueT>::numOfInputs() const
+{
+	return in_.size();
+}
+
+template <typename ValueT>
+std::size_t DataSetEntry<ValueT>::numOfOutputs() const
+{
+	return out_.size();
+}
+
+template <typename ValueT>
+ValueT DataSetEntry<ValueT>::getField(std::size_t index) const
+{
+	if (index >= (in_.size()+out_.size()))
+	{
+		FL_THROW2(std::invalid_argument, "Index is out-of-range");
+	}
+
+	return index < in_.size() ? in_.at(index)
+							  : out_.at(index-in_.size());
+}
+
+template <typename ValueT>
+ValueT DataSetEntry<ValueT>::getInput(std::size_t index) const
+{
+	if (index >= in_.size())
+	{
+		FL_THROW2(std::invalid_argument, "Index is out-of-range");
+	}
+
+	return in_.at(index);
+}
+
+template <typename ValueT>
+ValueT DataSetEntry<ValueT>::getOutput(std::size_t index) const
+{
+	if (index >= out_.size())
+	{
+		FL_THROW2(std::invalid_argument, "Index is out-of-range");
+	}
+
+	return out_.at(index);
+}
+
+
+///////////
+// DataSet
+///////////
+
+
+template <typename ValueT>
+DataSet<ValueT>::DataSet(std::size_t ni, std::size_t no)
+: ni_(ni),
+  no_(no)
+{
+}
+
+template <typename ValueT>
+void DataSet<ValueT>::add(const DataSetEntry<ValueT>& entry)
+{
+	if (entry.numOfInputs() != ni_)
+	{
+		FL_THROW2(std::invalid_argument, "Unexpected number of inputs in the entry");
+	}
+	if (entry.numOfOutputs() != no_)
+	{
+		FL_THROW2(std::invalid_argument, "Unexpected number of outputs in the entry");
+	}
+
+	entries_.push_back(entry);
+}
+
+template <typename ValueT>
+typename DataSet<ValueT>::EntryIterator DataSet<ValueT>::insert(typename DataSet<ValueT>::EntryIterator pos,
+																const DataSetEntry<ValueT>& entry)
+{
+	if (entry.numOfInputs() != ni_)
+	{
+		FL_THROW2(std::invalid_argument, "Unexpected number of inputs in the entry");
+	}
+	if (entry.numOfOutputs() != no_)
+	{
+		FL_THROW2(std::invalid_argument, "Unexpected number of outputs in the entry");
+	}
+
+	return entries_.insert(pos, entry);
+}
+
+template <typename ValueT>
+typename DataSet<ValueT>::EntryIterator DataSet<ValueT>::insert(typename DataSet<ValueT>::ConstEntryIterator pos,
+																const DataSetEntry<ValueT>& entry)
+{
+	if (entry.numOfInputs() != ni_)
+	{
+		FL_THROW2(std::invalid_argument, "Unexpected number of inputs in the entry");
+	}
+	if (entry.numOfOutputs() != no_)
+	{
+		FL_THROW2(std::invalid_argument, "Unexpected number of outputs in the entry");
+	}
+
+	return entries_.insert(pos, entry);
+}
+
+//template <typename ValueT>
+//typename DataSet<ValueT>::EntryIterator DataSet<ValueT>::insert(typename DataSet<ValueT>::ConstEntryIterator pos,
+//																  const DataSetEntry<ValueT>& entry)
+//{
+//	if (entry.numOfInputs() != ni_)
+//	{
+//		FL_THROW2(std::invalid_argument, "Unexpected number of inputs in the entry");
+//	}
+//	if (entry.numOfOutputs() != no_)
+//	{
+//		FL_THROW2(std::invalid_argument, "Unexpected number of outputs in the entry");
+//	}
+//
+//	return entries_.insert(pos, entry);
+//}
+
+template <typename ValueT>
+typename DataSet<ValueT>::EntryIterator DataSet<ValueT>::erase(typename DataSet<ValueT>::EntryIterator pos)
+{
+	return entries_.erase(pos);
+}
+
+template <typename ValueT>
+typename DataSet<ValueT>::EntryIterator DataSet<ValueT>::erase(typename DataSet<ValueT>::ConstEntryIterator pos)
+{
+	return entries_.erase(pos);
+}
+
+template <typename ValueT>
+void DataSet<ValueT>::set(const DataSetEntry<ValueT>& entry, std::size_t idx) const
+{
+	return entries_[idx] = entry;
+}
+
+template <typename ValueT>
+const DataSetEntry<ValueT>& DataSet<ValueT>::get(std::size_t idx) const
+{
+	return entries_.at(idx);
+}
+
+template <typename ValueT>
+std::size_t DataSet<ValueT>::size() const
+{
+	return entries_.size();
+}
+
+template <typename ValueT>
+bool DataSet<ValueT>::empty() const
+{
+	return entries_.empty();
+}
+
+//template <typename ValueT>
+//void DataSet<ValueT>::setNumOfInputs(std::size_t val)
+//{
+//	ni_ = val;
+//}
+
+template <typename ValueT>
+std::size_t DataSet<ValueT>::numOfInputs() const
+{
+	return ni_;
+}
+
+//template <typename ValueT>
+//void DataSet<ValueT>::setNumOfOutputs(std::size_t val)
+//{
+//	no_ = val;
+//}
+
+template <typename ValueT>
+std::size_t DataSet<ValueT>::numOfOutputs() const
+{
+	return no_;
+}
+
+//template <typename ValueT>
+//void DataSet<ValueT>::resize(std::size_t ni, std::size_t no)
+//{
+//	for (std::size_t i = 0,
+//					 ni = entries_.size();
+//		 i < ni;
+//		 ++i)
+//	{
+//		entries_[i].resize(ni, no);
+//	}
+//}
+
+template <typename ValueT>
+void DataSet<ValueT>::clear()
+{
+	entries_.clear();
+}
+
 } // Namespace fl
-
-
-#include "fl/dataset.tpp"
-
 
 #endif // FL_DATASET_H
